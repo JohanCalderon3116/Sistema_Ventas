c982dc49b723bc3cc3ebfb9d9ae3793bb9a926ea
Johan Calderon <yojancalderon3116@gmail.com>
2026-03-07 16:47:03 -0500

    Se crea el proyecto, se lo hace respondive y se crean 3 secciones en la app, se le dan estyles tambien

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { GlobalStyles } from "./index";
+import { Device } from "./styles/breakpoints";
+function App() {
+  return (
+    <Container>
+      <GlobalStyles></GlobalStyles>
+      <section className="contentSidebar">sidebar</section>
+      <section className="contentMenuambur">menuambur</section>
+      <section className="contentRouters ">roters</section>
+    </Container>
+  );
+}
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: black;
+  .contentSidebar {
+    display: none;
+    background-color: black;
+  }
+  .contentMenuambur {
+    position: absolute;
+    background-color: black;
+  }
+  .contentRouters {
+    background-color: black;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 88px 1fr;
+    .contentSidebar {
+      display: initial;
+    }
+    .contentMenuambur {
+      display: none;
+    }
+  }
+`;
+export default App;
